5f2d64399e030034edcda2b2412dfcaa75b1bad7
Facundo Veron <facu_ve_2016@hotmail.com>
2022-09-25 00:54:20 -0300

    Router2

--- Router.php
+++ Router.php
@@ -25,17 +25,13 @@ class Router
 
         // $auth = $_SESSION['login'] ?? null;
 
-        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'] ;
+        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
-            
-        // Dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
-        $splitURL = explode('?', $currentUrl);
-        // debuguear($splitURL);
-        
+
         if ($method === 'GET') {
-            $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
+            $fn = $this->getRoutes[$currentUrl] ?? null;
         } else {
-            $fn = $this->postRoutes[$splitURL[0]] ?? null;
+            $fn = $this->postRoutes[$currentUrl] ?? null;
         }
 
 
