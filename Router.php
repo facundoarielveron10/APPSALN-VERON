8d5f74a926867116037f2523dfa5572a065bf0d7
Facundo Veron <facu_ve_2016@hotmail.com>
2022-09-26 00:16:24 -0300

    Router Solucion?

--- Router.php
+++ Router.php
@@ -25,11 +25,12 @@ class Router
 
         // $auth = $_SESSION['login'] ?? null;
 
-        if ($_SERVER['PATH_INFO']) {
+        if (isset($_SERVER['PATH_INFO'])) {
             $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
         } else {
             $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         }
+        
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
